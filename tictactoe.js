8446e80f6536e1c87ccd1c783c6cdeb26648ba9d
Teresa <trothaar@users.noreply.github.com>
2017-04-01 19:39:53 -0400

    Add files via upload

---
+++ tictactoe.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+
+// Default human player is X; default computer player is O
+var turn = "X";
+var computerTurn = "O";
+
+// Array to store values that we will check later for a winner
+var turns = ["#", "#", "#", "#", "#", "#", "#", "#", "#"];
+
+});
